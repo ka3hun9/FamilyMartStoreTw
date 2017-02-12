627dc5a3d8179dbb3d2df729668eb1058de232fa
kamhung <403625070@qq.com>
2017-02-12 09:05:57 +0800

    create2

--- js/nowplaying.js
+++ js/nowplaying.js
@@ -1,5 +1,13 @@
 // 创建一个angular控制器模块
 angular.module('movieApp.nowplayingCtrl', [])
-    .controller('nowplayingCtrl', ['$scope', function($scope) {
-
+    .controller('nowplayingCtrl', ['$scope', '$http', function($scope, $http) {
+        $scope.name = "张三";
+        $scope.movie = {};
+        // $http 是angular中的ajax
+        $http({
+            url: 'data.json'
+        }).success(function(data) {
+            console.log(data);
+            $scope.movie = data;
+        })
     }]);
